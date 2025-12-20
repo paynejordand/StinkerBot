34dff23f2d6f4ca2dd00da40966b4464635b7624
Jordan <paynejordand+github@gmail.com>
2025-12-20 14:33:20 -0500

    Added refresh access token functionality. Added commands to get/set scores. Set Scores is only available to moderators/broadcaster

--- bot.js
+++ bot.js
@@ -19,6 +19,8 @@ const POI = {
 }
 
 var websocketSessionID;
+var scoreLink = "";
+
 
 // Start executing the bot from here
 (async () => {
@@ -31,24 +33,60 @@ var websocketSessionID;
 
 // WebSocket will persist the application loop until you exit the program forcefully
 
-async function getAuth() {
-  // https://dev.twitch.tv/docs/authentication/validate-tokens/#how-to-validate-a-token
-  console.log(process.env.OAUTH_TOKEN);
+async function validateToken() {
   let response = await fetch("https://id.twitch.tv/oauth2/validate", {
     method: "GET",
     headers: {
-      Authorization: "OAuth " + OAUTH_TOKEN,
+      Authorization: "OAuth " + process.env.OAUTH_TOKEN,
     },
   });
+  return response
+}
 
+async function refreshToken() {
+  let response = await fetch("https://id.twitch.tv/oauth2/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body:
+      "grant_type=refresh_token&client_id=" +
+      CLIENT_ID +
+      "&refresh_token=" +
+      process.env.REFRESH_TOKEN +
+      "&client_secret=" +
+      process.env.CLIENT_SECRET,
+  });
+
+  return response
+}
+
+async function getAuth() {
+  // https://dev.twitch.tv/docs/authentication/validate-tokens/#how-to-validate-a-token
+  let response = await validateToken();
+  
   if (response.status != 200) {
+    // let data = await response.json();
+    // console.log(response);
+    // console.error(
+    //   "Token is not valid. /oauth2/validate returned status code " +
+    //     response.status
+    // );
+    // console.error(data);
+    console.log("-- Attempting to refresh token --")
+    let response = await refreshToken()
     let data = await response.json();
-    console.error(
-      "Token is not valid. /oauth2/validate returned status code " +
-        response.status
-    );
-    console.error(data);
-    process.exit(1);
+    if (response.status != 200)
+    {
+      console.log(response);
+      console.error(
+        "Token is not valid. /oauth2/token returned status code " +
+          response.status
+      );
+      console.error(data);
+      process.exit(1);
+    }
+    process.env.OAUTH_TOKEN = data.access_token
   }
 
   console.log("Validated token.");
@@ -78,63 +116,137 @@ function sendWebSocketMessage(message) {
     };
 }
 
+function inRange(val, x, y) {
+  return val >= x && val <= y;
+}
+
 function handleSpectateCommand(messageText) {
     let parts = messageText.split(" ");
     console.log(parts);
 
     if (parts.length == 1) {
-      sendChatMessage("Usage: !spectate <player|poi> <name|number (1-6)>");
+      sendChatMessage("Usage: !spectate <name|number (1-6)>");
       return;
     }
 
-    if (parts[1] == "player") {
+    if (inRange(parseInt(parts[1]), 1, 6)) {
+      let targetPOI = POI[parseInt(parts[1])] || null;
+      if (targetPOI) {
+        sendChatMessage("Switching to player of interest: " + targetPOI);
+        sendWebSocketMessage(
+          JSON.stringify({
+            changeCam: { poi: parseInt(parts[1]) },
+          })
+        );
+      } else {
+        sendChatMessage(
+          "POI number " + parts[2] + " is not valid. Must be between 1 and 6."
+        );
+      }
+    }
+    else {
       let playerName = "";
-      if (parts.length > 3) {
-        for (let i = 2; i < parts.length; i++) {
+      if (parts.length > 2) {
+        for (let i = 1; i < parts.length; i++) {
           playerName += parts[i];
           if (i != parts.length - 1) {
             playerName += " ";
           }
         }
       } else {
-        playerName = parts[2];
+        playerName = parts[1];
       }
 
       sendChatMessage("Switching to player: " + playerName);
-      sendWebSocketMessage(JSON.stringify({
-        changeCam: { name: playerName },
-      }));
-
-    } else if (parts[1] == "poi") {
-      if (parseInt(parts[2])) {
-        let targetPOI = POI[parseInt(parts[2])] || null;
-        if (targetPOI) {
-          sendChatMessage("Switching to player of interest: " + targetPOI);
-          sendWebSocketMessage(
-            JSON.stringify({
-              changeCam: { poi: parseInt(parts[2]) },
-            })
-          );
-        } else {
-          sendChatMessage("POI number " + parts[2] + " is not valid. Must be between 1 and 6.");
-        }
-      }
+      sendWebSocketMessage(
+        JSON.stringify({
+          changeCam: { name: playerName },
+        })
+      );
     }
 }
 
-function handleSwapCommand()
-{
+function handleSwapCommand() {
     sendChatMessage("Swapping to next instance of player damage!");
     sendWebSocketMessage(JSON.stringify({
       "swapCam": {},
     }));
 }
 
-function handleHelpCommand()
-{
+function handleHelpCommand() {
     sendChatMessage(
       "Commands: !spectate !swap"    
     );
+}
+
+async function handleScoreCommand() {
+  if (!scoreLink) {
+    sendChatMessage("No valid link for scores set.");
+    return;
+  }
+  try {
+    let response = await fetch(scoreLink);
+    if (response.status !== 200) {
+      sendChatMessage("Invalid link set.");
+      return;
+    }
+    sendChatMessage(await response.text());
+  }
+  catch (error) {
+    console.error(error);
+    return;
+  }
+}
+
+async function isModerator(chatterId) {
+  let response = await fetch("https://api.twitch.tv/helix/moderation/moderators?broadcaster_id=" 
+    + process.env.CHANNEL_ID
+    + "&user_id=" + chatterId, {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + process.env.OAUTH_TOKEN,
+      'Client-Id': process.env.CLIENT_ID
+    }
+  });
+  if (response.status != 200) {
+    let data = response.json()
+    console.log(response);
+    console.error(
+      "Request failed " +
+        response.status
+    );
+    console.error(data);
+    return false;
+  }
+
+  let data = await response.json();
+  if (data.data.length == 0) return false;
+  return true;
+}
+
+function isBroadcaster(chatterId) {
+  return chatterId == process.env.CHANNEL_ID;
+}
+
+async function handleSetScoreCommand(chatterId, newScoreLink) {
+  // check if user is a moderator
+  if (!await isModerator(chatterId) && !isBroadcaster(chatterId)) {
+    sendChatMessage("Only mods in this channel can use this command!");
+    return;
+  }
+
+  try {
+    let response = await fetch(newScoreLink);
+    if (response.status !== 200) {
+      sendChatMessage("Invalid link.");
+      return;
+    }
+  } catch (error) {
+    console.error(error);
+    return;
+  }
+  scoreLink = newScoreLink;
+  sendChatMessage("Score link set to " + newScoreLink);
 }
 
 function handleWebSocketMessage(data) {
@@ -163,6 +275,13 @@ function handleWebSocketMessage(data) {
           else if (command == "!help") {
             handleHelpCommand();
           }
+          else if (command == "!score") {
+            handleScoreCommand();
+          }
+          else if (command == "!setscore") {
+            handleSetScoreCommand(data.payload.event.chatter_user_id, 
+              data.payload.event.message.text.trim().split(" ")[1]);
+          }
         break;
       }
     break;
@@ -173,7 +292,7 @@ async function sendChatMessage(chatMessage) {
   let response = await fetch("https://api.twitch.tv/helix/chat/messages", {
     method: "POST",
     headers: {
-      Authorization: "Bearer " + OAUTH_TOKEN,
+      Authorization: "Bearer " + process.env.OAUTH_TOKEN,
       "Client-Id": CLIENT_ID,
       "Content-Type": "application/json",
     },
@@ -200,7 +319,7 @@ async function registerEventSubListeners() {
     {
       method: "POST",
       headers: {
-        Authorization: "Bearer " + OAUTH_TOKEN,
+        Authorization: "Bearer " + process.env.OAUTH_TOKEN,
         "Client-Id": CLIENT_ID,
         "Content-Type": "application/json",
       },
